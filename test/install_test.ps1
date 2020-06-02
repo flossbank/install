ca2667b85405b59680dccab2dbde011db42db206
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-06-02 10:39:33 -0700

    Add ps test

--- test/install_test.ps1
+++ test/install_test.ps1
@@ -2,4 +2,26 @@
 
 $ErrorActionPreference = 'Stop'
 
-Write-Output "Testing"
+# set staging api endpoint so we can use bogus install token
+$WinConfigPath = Join-Path $Home "AppData" "Roaming" "flossbank-nodejs" "Config"
+New-Item $WinConfigPath -ItemType Directory
+$WinConfig = Join-Path $WinConfigPath "config.json"
+Write-Output '{"apiHost":"https://api.flossbank.io"}' > $WinConfig
+
+$FLOSSBANK_INSTALL_TOKEN = "cf667c9381f7792bfa772025ff8ee93b89d9a757e6732e87611a0c34b48357d1"
+
+# install the latest version at the default location
+$DefaultLocation = Join-Path $Home ".flossbank"
+Remove-Item -Force -Recurse $DefaultLocation
+$FLOSSBANK_INSTALL = ""
+.\install.ps1
+$Exe = Join-Path $DefaultLocation "bin" "flossbank"
+Start-Process $Exe -Wait -NoNewWindow -PassThru
+
+# install to a custom location
+$CustomLocation = Join-Path $Home "flossbank-custom"
+Remove-Item -Force -Recurse $CustomLocation
+$FLOSSBANK_INSTALL = $CustomLocation
+.\install.ps1
+$Exe = Join-Path $CustomLocation "bin" "flossbank"
+Start-Process $Exe -Wait -NoNewWindow -PassThru
