25b6cb8a4a922efbb17b9857e8b1d5085aead2bf
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-06-02 12:00:24 -0700

    Bugfix PS test

--- test/install_test.ps1
+++ test/install_test.ps1
@@ -3,12 +3,24 @@
 $ErrorActionPreference = 'Stop'
 
 # set staging api endpoint so we can use bogus install token
+$LinuxConfigPath = if ($XDG_CONFIG_HOME) {
+  Join-Path $XDG_CONFIG_HOME ".config" "flossbank-nodejs"
+} else {
+  Join-Path $Home ".config" "flossbank-nodejs"
+}
+$MacConfigPath = Join-Path $Home "Library" "Preferences" "flossbank-nodejs"
 $WinConfigPath = Join-Path $Home "AppData" "Roaming" "flossbank-nodejs" "Config"
-New-Item $WinConfigPath -ItemType Directory
+New-Item $LinuxConfigPath -ItemType Directory | Out-Null
+New-Item $MacConfigPath -ItemType Directory | Out-Null
+New-Item $WinConfigPath -ItemType Directory | Out-Null
+$LinuxConfig = Join-Path $LinuxConfigPath "config.json"
+$MacConfig = Join-Path $MacConfigPath "config.json"
 $WinConfig = Join-Path $WinConfigPath "config.json"
+Write-Output '{"apiHost":"https://api.flossbank.io"}' > $LinuxConfig
+Write-Output '{"apiHost":"https://api.flossbank.io"}' > $MacConfig
 Write-Output '{"apiHost":"https://api.flossbank.io"}' > $WinConfig
 
-$FLOSSBANK_INSTALL_TOKEN = "cf667c9381f7792bfa772025ff8ee93b89d9a757e6732e87611a0c34b48357d1"
+$env:FLOSSBANK_INSTALL_TOKEN = "cf667c9381f7792bfa772025ff8ee93b89d9a757e6732e87611a0c34b48357d1"
 
 # install the latest version at the default location
 $DefaultLocation = Join-Path $Home ".flossbank"
